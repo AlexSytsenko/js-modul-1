67ac4a3802a9680ca102c71d65a941e72adcb815
AlexSytsenko <sytsenko@gmail.com>
2020-10-30 11:40:35 +0200

    создает задачи первого модуля

---
+++ js/practic.js
@@ -0,0 +1,86 @@
+"use strick";
+
+// userNumber = prompt('Введите число');
+
+// let total = 0;
+
+// for (let i = 0;  )
+
+
+
+
+
+// const number = 10;
+
+// for (let i = 0; i < number; i += 1) {
+//   if (i % 2 === 0) {
+//     continue;
+//   }
+
+//   console.log("Нечетное i: ", i); // 1, 3, 5, 7, 9
+// }
+
+
+let input = prompt('Введите число для проверки деления на 3');
+
+if (input === null) {
+  console.log('Отмена пользователя! Спасибо за внимание');
+}
+
+
+if (input % 3 === 0)
+
+
+
+// let password = "";
+
+// do {
+//   password = prompt("Введите пароль длиннее 4-х символов", "");
+// } while (password.length < 5);
+
+// console.log("Ввели пароль: ", password);
+// let total = 0;
+
+// while (true) {
+//   let userNumber = prompt("Введите число");
+
+//   if (userNumber === null) {
+//     console.log("Отменено пользователем");
+//     break;
+//   }
+ 
+
+//   userNumber = Number(userNumber);
+
+//   const notANumber = Number.isNaN(userNumber);
+
+//   if (notANumber) {
+//     console.log("Было введено не число пропускаем это значение");
+//     continue;
+//   }
+
+//   total += userNumber;
+// }
+
+// console.log(`Общая сума ${ total }`);
+
+
+
+// const number = 5;2
+
+//  let total = 0;
+
+// // for (let i = 1; i <= number; i += ) {
+// //   total += i;
+// // }
+
+// // console.log(`total ${total}`);
+
+// let i = 1;
+
+// while (i <= number) {
+//   total += i
+//   i += 1;
+// }
+
+// console.log(`total ${total}`);
